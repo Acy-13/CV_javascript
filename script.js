9386c028e44c06f9eb7716b783ceeaa6ef4453e6
Acy-13 <alina.vaittinen@gmail.com>
2023-01-31 16:32:45 +0100

    added javascript tpofooter links

--- script.js
+++ script.js
@@ -15,8 +15,32 @@ function myFunction() {
     
 //     spans[i].style.color=colors[i];
 
-
+//Dark mode-knapp
 function switchMode() {
   let element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+//Opacititet på footer-ikonerna, försvinner lätt och kommer tillbaka
+let foelm = document.getElementById("footer-icons");
+
+let opac = 0.1;
+let opacup=true;
+renderLoop();
+
+      function renderLoop() {
+        setTimeout(function() {
+          if(opacup==true)
+             {opac+=0.1;
+              if(opac>=1)opacup=false;
+             }
+         if(opacup==false)
+             {opac-=0.1;
+              if(opac<=0.3) opacup=true;
+             }
+ 
+        foelm.style.opacity = opac;
+       
+       window.requestAnimationFrame(renderLoop); 
+        },300);
+      }
